e73209a9f046d5dae449b9d16621dc72e839a3da
Rajshankar13 <rajshankarkhattar13@gmail.com>
2020-05-11 12:46:56 +0530

    Rating

--- movie_reviews/movie_reviews/spiders/metacritic_reviews.py
+++ movie_reviews/movie_reviews/spiders/metacritic_reviews.py
@@ -6,7 +6,7 @@ class MetacriticReviewsSpider(scrapy.Spider):
 	name = "metacritic_reviews"
 
 	def start_requests(self):
-		url = 'http://www.metacritic.com/movie/avengers-endgame/user-reviews'
+		url = input()
 		yield scrapy.Request(url = url, callback = self.parse)
 
 	def parse(self, response):
@@ -18,7 +18,7 @@ class MetacriticReviewsSpider(scrapy.Spider):
 				'expanded_content': selector.xpath("span/span[@class='blurb blurb_expanded']/text()").extract()
 			}
 
-		next_page = response.selector.xpath("//span[@class='flipper next']/a/@href").extract_first()
-		if next_page is not None:
-			next_page_link = response.urljoin(next_page)
-			yield scrapy.Request(url = next_page_link, callback = self.parse)		
+		# next_page = response.selector.xpath("//span[@class='flipper next']/a/@href").extract_first()
+		# if next_page is not None:
+		# 	next_page_link = response.urljoin(next_page)
+		# 	yield scrapy.Request(url = next_page_link, callback = self.parse)		
--- review_generator_with_lemmatization.py
+++ review_generator_with_lemmatization.py
@@ -1,10 +1,10 @@
 import json
 import re
 from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 
 
-with open('movie_reviews/joker-reviews.json', ) as f:
+with open('movie_reviews/wall-street.json', ) as f:
 	reviews = json.load(f)
 
 all_reviews = list()
@@ -16,21 +16,21 @@ for review in reviews:									#reviews is a list of dictionaries
 			if(val != ' '):								#Checking if content is not null
 				all_reviews.append(val.split())
 
-#Calculating ratings based on the scores given by the user at the site
-with open('movie_reviews/joker-ratings.json', ) as f:
-	ratings = json.load(f)
+# #Calculating ratings based on the scores given by the user at the site
+# with open('movie_reviews/joker-ratings.json', ) as f:
+# 	ratings = json.load(f)
 
-all_ratings = list()
+# all_ratings = list()
 
-for rating in ratings:
-	for value in rating.values():
-		value = int(value)
-		all_ratings.append(value)
+# for rating in ratings:
+# 	for value in rating.values():
+# 		value = int(value)
+# 		all_ratings.append(value)
 
-total_sum = 0
-for rating in all_ratings:
-	total_sum += rating
-#----------------------------------------------------------------------
+# total_sum = 0
+# for rating in all_ratings:
+# 	total_sum += rating
+# #----------------------------------------------------------------------
 
 english_stop_words = stopwords.words('english')
 english_stop_words.remove("not")
@@ -46,36 +46,33 @@ for review in all_reviews:
 	if(len(list1) != 0):
 		cleaned_reviews.append(list1)					#To remove empty lists
 
-#Perform Stemming
-def stemming(corpa):
+#Perform lemmatization
+def lemmatization(corpa):
 	text = list()
-	porter_stemmer = PorterStemmer()
+	lemmatizer = WordNetLemmatizer()
 	for word in corpa:
-		text.append(porter_stemmer.stem(word))
+		text.append(lemmatizer.lemmatize(word))
 	return text
 
-stemmed_reviews = list()
+lemmatized_reviews = list()
 for review in cleaned_reviews:
-	stemmed_reviews.append(stemming(review))
+	lemmatized_reviews.append(lemmatization(review))
 
 #Reading positive_words.txt and negative_words.txt
 positive_words, negative_words = list(), list()
 with open('positive-words.txt', ) as f:
-	positive_words = stemming(f.read().split())
+	positive_words = lemmatization(f.read().split())
 	positive_words = set(positive_words)				#Removing repeated words
 	positive_words = list(positive_words)				#Converting back to list
 
 with open('negative-words.txt', ) as f:
-	negative_words = stemming(f.read().split())
+	negative_words = lemmatization(f.read().split())
 	negative_words = set(negative_words)				#Removing repeated words
 	negative_words = list(negative_words)				#Converting back to list
 
 #Labelling the reviews
 positive_reviews, negative_reviews, neutral_reviews, flag = 0, 0, 0, 0
-most_positive, most_negative = 0, 0
-review_index, positive_index, negative_index = -1, 0, 0
-for review in stemmed_reviews:
-	review_index += 1
+for review in lemmatized_reviews:
 	positive_word_count = 0
 	negative_word_count = 0
 
@@ -101,16 +98,10 @@ for review in stemmed_reviews:
 			negative_word_count += 1
 
 	if(positive_word_count > negative_word_count):
-		if((positive_word_count - negative_word_count) > most_positive):
-			most_positive = positive_word_count
-			positive_index = review_index
 		review.append(1)								#Label review as positive
 		positive_reviews += 1
 
 	elif(positive_word_count < negative_word_count):
-		if((negative_word_count - positive_word_count) > most_negative):
-			most_negative = negative_word_count
-			negative_index = review_index
 		review.append(0)								#Label review as negative
 		negative_reviews += 1
 
@@ -121,14 +112,11 @@ for review in stemmed_reviews:
 formula1 = (positive_reviews + 0.5 * neutral_reviews) / (positive_reviews + negative_reviews)
 formula2 = (positive_reviews + 0.5 * neutral_reviews) / (positive_reviews + negative_reviews + 0.5 * neutral_reviews)
 
-# print(stemmed_reviews)
-print("Most Positive Review: ", cleaned_reviews[positive_index])
-print("---------------------------------------------------------")
-print("Most Negative Review: ", cleaned_reviews[negative_index])
-print("Total reviews: ", len(stemmed_reviews), "Positive Reviews: ", positive_reviews, 
+# print(lemmatized_reviews)
+print("Total reviews: ", len(lemmatized_reviews), "Positive Reviews: ", positive_reviews, 
 	"Negative Reviews: ", negative_reviews, "Neutral Reviews: ", neutral_reviews)
 print("Formula 1: ", formula1, "Formula 2: ", formula2)
 # print("Rating by user score: ", total_sum / len(ratings))
-# for review in stemmed_reviews:
+# for review in lemmatized_reviews:
 # 	if(review[-1] == 2):
 # 		print(review)
